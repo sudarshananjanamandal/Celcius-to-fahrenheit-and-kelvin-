31e6b87c2d9c546c9b15556aa51c4c11c751b653
Sudarshan Mandal <sudarshananjanamandal@gmail.com>
2020-07-01 11:20:51 +0530

    Add files via upload
    this python program can be used to convert Celcius into Fahrenheit and Kelvin

---
+++ celcius_to_fahrenheit.py
@@ -0,0 +1,4 @@
+temp=float(input("Enter the temperature in celcius: "))
+fahren=(temp*9/5)+32
+kelvin=(temp+273.15)
+print("The temperature in Fahrenheit is {} and temperature in kelvin is {}".format(fahren,kelvin))
